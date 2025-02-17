c2f983489fb8a6744b2b15355a6515435ec3564b
Huan He <140455389+huanhe4096@users.noreply.github.com>
2025-02-17 09:40:18 -0500

    update 1.0.1

--- web/src/DataStore.js
+++ web/src/DataStore.js
@@ -8,7 +8,7 @@ import { pubmed } from './utils/pubmed';
 
 export const useDataStore = defineStore('jarvis', {
 state: () => ({
-    version: '1.0.0',
+    version: '1.0.1',
     current_page: '',
     config: {
         keywords: [
